2513a0eee4fa55fc1687e4c95efd9ac4cfec9dbd
tmlaurencio <73615455+tmlaurencio@users.noreply.github.com>
2021-03-04 21:28:08 -0600

    Pared down fws
    Cut back on arguments to function

--- fws.py
+++ fws.py
@@ -3,24 +3,20 @@ Possible forward selection for linear regression
 '''
 import numpy as np
 import pandas as pd
-from sklearn.linear_model import LassoCV, LinearRegression
+from sklearn.linear_model import LinearRegression
 from sklearn.feature_selection import SequentialFeatureSelector
 from sklearn.metrics import r2_score
 
-def forward_selection(dat, dependent, n_features, model = LinearRegression(),
-                      score_method = 'r2'):
+def forward_selection(dat, dependent):
     """
     Outputs labels of independent variables which are deemed most important
     through process of forward selection
 
     Inputs:
     dat (Pandas DataFrame): a Pandas DataFrame housing source data for regression
-    dependent (str): the name of the dependent variable
-    n_features (int): the number of features to select
-    model (function): a linear model.  Defaults to LassoCV()
-    score_method (str): a scoring method for each intermediate model during
-                        the forward selection process.  Default is r2 for 
-                        r^2 scoring
+    dependent (Pandas Series): The column containing the dependent data
+    model (function): a linear model.  Defaults to LinearRegression()
+ 
     
     Output:
     (dict) a dictionary of the form {independent variable: coefficient in linear model}
@@ -28,33 +24,31 @@ def forward_selection(dat, dependent, n_features, model = LinearRegression(),
     """
     var_order = dict()
     
-    X = dat.drop(dependent, axis = 1) 
-    y = dat[dependent] 
-    fitted = model.fit(X, y) #model has to be fit to all data first before being 
+    fitted = model.fit(dat, dependent) #model has to be fit to all data first before being 
                              #used by SequentialFeatureSelector
 
-    forwards = SequentialFeatureSelector(fitted, n_features_to_select = n_features,
-                                            scoring = score_method, 
-                                            direction = 'forward').fit(X, y)
+    forwards = SequentialFeatureSelector(fitted, n_features_to_select = dat.shape[1], #number of independent variables
+                                            scoring = 'r2', 
+                                            direction = 'forward').fit(dat, dependent)
     #documentation: https://scikit-learn.org/stable/modules/generated/sklearn.feature_selection.SequentialFeatureSelector.html#sklearn.feature_selection.SequentialFeatureSelector.get_params 
 
     
     #uses boolean mask to get selected variables
-    fit_vars = np.array(X.columns)[forwards.support_] 
+    fit_vars = np.array(dat.columns)[forwards.support_] 
     
     #X_cut contains the data from X which corresponds to the variables chosen
     #through forward selection.  It's a numpy array and not a Pandas Dataframe,
     #so the column names that the data corresponds to aren't preserved
-    X_cut = forwards.transform(X) 
+    X_cut = forwards.transform(dat) 
     
     #The coefficients for each parameter in the linear model
-    finmod = model.fit(X_cut, y)
+    finmod = model.fit(X_cut, dependent)
     
     for ind, val in enumerate(list(fit_vars)):
         var_order[val] = finmod.coef_[ind]
     
     var_order['Intercept'] = finmod.intercept_
-    var_order['Model Score (r2)'] = finmod.score(X_cut, y)
+    var_order['Model Score (r2)'] = finmod.score(X_cut, dependent)
 
     return var_order
 
