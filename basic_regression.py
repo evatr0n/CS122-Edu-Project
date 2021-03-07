e4f36db047abcd6bc9046912d9c439e52f63cdeb
Jake Underland <jakez0626@gmail.com>
2021-03-08 04:28:57 +0900

    returning pandas df with necessary info

--- basic_regression.py
+++ basic_regression.py
@@ -11,6 +11,9 @@ def run_regression(dependent_df, independent_df):
     dependent_df is a df of one column featuring the dependent variable.
     independent_df is a df of several columns featuring columns with each 
     of the independent variables. 
+
+    Returns pandas dataframe with policies, R2 and Intercept as index and 
+    coefficients and values that correspond. Useful for visualization.
     '''
     X = independent_df.values
     y = dependent_df.values
@@ -21,7 +24,7 @@ def run_regression(dependent_df, independent_df):
     coef_df = pd.DataFrame(ols.coef_, independent_df.columns, columns=["values"])
     appendage = pd.DataFrame([ols.intercept_, ols.score(X, y)], ["Intercept", "R2"], columns=["values"])  #values = coefficients
 
-    return pd.concat([coef_df, appendage], ignore_index = True)
+    return pd.concat([coef_df, appendage], ignore_index = True) # use .loc to iterate
 
 
 #tester_df = pd.concat([dfdic["nctq_20{}".format(year)] for year in range(18, 22)], axis = 1)
