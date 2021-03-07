abb01c3555cae2a043e0f8dce3811ce3474a9f33
Jake Underland <jakez0626@gmail.com>
2021-03-08 04:25:41 +0900

    returning pandas df with necessary info

---
+++ basic_regression.py
@@ -0,0 +1,27 @@
+### Basic regression function
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+#import statsmodels.formula.api as sm
+
+def run_regression(dependent_df, independent_df):
+    '''
+    dependent_df is a df of one column featuring the dependent variable.
+    independent_df is a df of several columns featuring columns with each 
+    of the independent variables. 
+    '''
+    X = independent_df.values
+    y = dependent_df.values
+    ols = LinearRegression()
+    ols.fit(X, y)
+    #intercept = ols.intercept_
+    #R2 = ols.score(X, y)
+    coef_df = pd.DataFrame(ols.coef_, independent_df.columns, columns=["values"])
+    appendage = pd.DataFrame([ols.intercept_, ols.score(X, y)], ["Intercept", "R2"], columns=["values"])  #values = coefficients
+
+    return pd.concat([coef_df, appendage], ignore_index = True)
+
+
+#tester_df = pd.concat([dfdic["nctq_20{}".format(year)] for year in range(18, 22)], axis = 1)
