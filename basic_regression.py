3c8bb4354949d0018dd2cc69f77392fa1e540715
Jake Underland <jakez0626@gmail.com>
2021-03-09 07:34:25 +0900

    calculating individual R2 scores

--- basic_regression.py
+++ basic_regression.py
@@ -6,7 +6,7 @@ import pandas as pd
 from sklearn.linear_model import LinearRegression
 #import statsmodels.formula.api as sm
 
-def run_regression(dependent_df, independent_df):
+def run_regression(independent_df, dependent_df):
     '''
     dependent_df is a df of one column featuring the dependent variable.
     independent_df is a df of several columns featuring columns with each 
@@ -17,14 +17,34 @@ def run_regression(dependent_df, independent_df):
     '''
     X = independent_df.values
     y = dependent_df.values
-    ols = LinearRegression()
+    ols = LinearRegression(normalize=True)
+    if isinstance(independent_df, pd.Series):
+        X = X.reshape(-1, 1)
+        var_names = [independent_df.name]
+    else:
+        var_names = independent_df.columns
     ols.fit(X, y)
     #intercept = ols.intercept_
     #R2 = ols.score(X, y)
-    coef_df = pd.DataFrame(ols.coef_, independent_df.columns, columns=["values"])
+    coef_df = pd.DataFrame(ols.coef_, var_names, columns=["values"])
     appendage = pd.DataFrame([ols.intercept_, ols.score(X, y)], ["Intercept", "R2"], columns=["values"])  #values = coefficients
 
-    return pd.concat([coef_df, appendage], ignore_index = True) # use .loc to iterate
+    return pd.concat([coef_df, appendage]) # use .loc to iterate
 
 
 #tester_df = pd.concat([dfdic["nctq_20{}".format(year)] for year in range(18, 22)], axis = 1)
+
+
+def cutoff_R2(avg_nctq_df, dependent_df, R2):
+    policyr2 = []
+    for i, policy in enumerate(avg_nctq_df.columns):
+        X = avg_nctq_df[policy]
+        y = dependent_df
+        regression = run_regression(X, y) 
+        r2 = regression.loc["R2", "values"]
+        if r2 > R2:
+            intercept = regression.loc[policy, "values"]
+            policyr2.append((policy, intercept, r2))
+    #sorted(policyr2, key=lambda tup:tup[2])
+
+    return policyr2
--- program_hq.py
+++ program_hq.py
@@ -2,11 +2,28 @@ import crawler
 import default_stat_analysis
 import basic_regression
 import nces
+import numpy as np
+import numbers
+import fws
+import pandas as pd
 
 print("Loading NCTQ Teaching Policy data.....")
-df_dic = crawler.crawl_nctq()
+nctqdic_original = crawler.crawl_nctq()
 print("Loading NCES Educational Outcome data.....")
-nces_df = nces.final
+nces_original = nces.final.sort_index()
+nces_filled = nces.filled.sort_index()
+
+nctqdic_filled = crawler.fill_na(nctqdic_original)
+avg_nctq = crawler.average_df(nctqdic_filled).sort_index()  # Use this for default scores.
+
+# computing individual weights of policies through bivariate regression
+weights_per_outcome = {}
+for outcome in nces.filled.columns:
+    weights_per_outcome[outcome] = basic_regression.cutoff_R2(avg_nctq, nces.filled[outcome], 0.1)
+
+    fws.forward_selection(avg_nctq[[tup[0] for tup in weights_per_outcome[outcome]])
 
 
 
+for y in nces.filled.columns:
+    fws.forward_selection(avg_nctq, y)
