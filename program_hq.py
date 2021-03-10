0d669825d241c6cf4d5aa971913131b2ea7dfe10
Jake Underland <jakez0626@gmail.com>
2021-03-11 02:28:44 +0900

    centering data in nctq

--- program_hq.py
+++ program_hq.py
@@ -1,4 +1,4 @@
-import crawler
+import nctq
 import default_stat_analysis
 import basic_regression
 import nces
@@ -8,22 +8,13 @@ import fws
 import pandas as pd
 
 print("Loading NCTQ Teaching Policy data.....")
-nctqdic_original = crawler.crawl_nctq()
+nctqdic_original = nctq.crawl_nctq()
 print("Loading NCES Educational Outcome data.....")
 nces_original = nces.final.sort_index()
 nces_filled = nces.filled.sort_index()
 
-nctqdic_filled = crawler.fill_na(nctqdic_original)
-avg_nctq = crawler.average_df(nctqdic_filled).sort_index()  # Use this for default scores.
+nctqdic_filled = nctq.fill_na(nctqdic_original)
+avg_nctq = nctq.average_df(nctqdic_filled).sort_index()  
+centered_avg = nctq.center_df(avg_nctq) # Use this for default scores.
 
 # computing individual weights of policies through bivariate regression
-weights_per_outcome = {}
-for outcome in nces.filled.columns:
-    weights_per_outcome[outcome] = basic_regression.cutoff_R2(avg_nctq, nces.filled[outcome], 0.1)
-
-    fws.forward_selection(avg_nctq[[tup[0] for tup in weights_per_outcome[outcome]])
-
-
-
-for y in nces.filled.columns:
-    fws.forward_selection(avg_nctq, y)
