c18b2befe053cdc42da26f1af350bb966e371ae2
tmlaurencio <t.m.laurencio@gmail.com>
2021-03-11 10:01:30 -0600

    fixed dataframe of base data+regression results, still need to change variable used for X-axis to variable with highest score

--- ui_plot.py
+++ ui_plot.py
@@ -1,4 +1,5 @@
 from matplotlib import pyplot as plt
+import pandas as pd
 
 #for testing
 import program_hq as phq
@@ -21,9 +22,10 @@ def graph(dat, outcome, linmod):
     (Figure, DataFrame) The plotted model, data and model coefficient dataframes, 
                         and the correlation matrix dataframe 
     """
+    #could probably do this operation with DataFrame.apply but unsure
     temp = dat.copy()
     for col in dat.columns:
-        if col in linmod.keys(): 
+        if col in linmod.keys(): #assumes no duplicates in column names
             temp[col] = temp[col].apply(lambda x: x * linmod[col])
     
     y_pred = temp.sum(axis = 1) #series containing predicted values for outcome
@@ -32,24 +34,27 @@ def graph(dat, outcome, linmod):
     #should change to variable with highest score from bivariate model
     strong = max(linmod, key = linmod.get) 
     
-    #scatterplot
+
+    #scatterplot with regression line
     plt.plot(dat[strong], y_pred)
     plt.scatter(dat[strong], outcome)
-    
     plt.xlabel(strong)
     plt.ylabel(outcome.name)
-    plt.title("{j} vs. {a}".format(j = strong, a = outcome.name))
+    plt.title("{a} vs. {b}".format(a = strong, b = outcome.name))
 
-    #visualization of base dataframe with outcome data added 
-    #as rightmost column; unsure if these appear as tables in UI
-    print(dat.insert(len(dat.columns), outcome.name, outcome))
 
-    #visualization of regression coefficients
-    
-    #since each value i is a scalar, need to convert values to arraylike
-    #to convert to dataframe
+    #souce_df contains base data, actual outcome values, and predicted outcome
+    #values from the regression.  The first column from the right contains the 
+    #outcome data while the rightmost column contains the regression predictions 
+    #for the outcome data
+    source_dfs = dat.insert(len(dat.columns), outcome.name, outcome)
+    source_dfs = source_dfs.insert(len(source_dfs.columns), outcome.name + ' pred', y_pred)
+    print(source_dfs)
+
+
+    #visualization of regression coefficients: since each value i is a scalar, 
+    #need to convert values to arraylike to convert to dataframe
     lst_dict = {k: [i] for k, i in linmod.items()}
-
     print(pd.DataFrame.from_dict(lst_dict))
 
 
