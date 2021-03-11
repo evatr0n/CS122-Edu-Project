12f6973f04f42cee41b18b4a5d9b158f277f4e7b
jrgill-coder <78323703+jrgill-coder@users.noreply.github.com>
2021-03-10 22:04:17 -0600

    Add files via upload

--- nces.py
+++ nces.py
@@ -4,12 +4,77 @@ import pandas as pd
 import numpy as np
 import re
 from sklearn import preprocessing 
+import scipy
+import os
 
 # FOR GROUPMATES
-# nces.final produces the table with the raw data. nces.filled is the same frame
-# but with all nan values filled in and money values ('$69,420') converted to
-# strings ('69420') that pandas can automatically convert to ints. 
+# nces.final is the table you want but you can also just use the csv that i
+# uploaded as well. still some bugs to fix but it's real close.
 
+
+data = {"https://nces.ed.gov/programs/digest/d19/tables/dt19_203.90.asp?current=yes" :
+[(0, "2007 Average Daily Attendance %"), (6, "2011 Average Daily Attendance %")],
+
+"https://nces.ed.gov/programs/digest/d19/tables/dt19_204.90.asp?current=yes":
+[(0, "2004 Students Enrolled in Gifted Programs %"), 
+(3, "2006 Students Enrolled in Gifted Programs %"), 
+(6, "2011 Students Enrolled in Gifted Programs %"), 
+(7, "2013 Students Enrolled in Gifted Programs %")],
+
+
+
+"https://nces.ed.gov/programs/digest/d19/tables/dt19_219.85b.asp?current=yes": 
+[(0, "2018 Percentage of HS Drop Outs Age 16-24")],
+
+"https://nces.ed.gov/programs/digest/d19/tables/dt19_221.72.asp?current=yes":
+[(10, "2019 8th Grade Reading Scores")],
+
+
+
+"https://nces.ed.gov/programs/digest/d19/tables/dt19_211.30.asp?current=yes":
+[(32, "2007 Average Base Teacher Salary w/ Bachelors"),
+(34, "2011 Average Base Teacher Salary w/ Bachelors"),
+(40, "2017 Average Base Teacher Salary w/ Bachelors")],
+
+
+"https://nces.ed.gov/programs/digest/d19/tables/dt19_211.40.asp?current=yes":
+[(34, "2007 Average Base Teacher Salary w/ Masters Constant Dollars"),
+(36, "2011 Average Base Teacher Salary w/ Masters Constant Dollars"),
+(42, "2017 Average Base Teacher Salary w/ Masters Constant Dollars")],
+
+
+ "https://nces.ed.gov/programs/digest/d20/tables/dt20_213.40.asp?current=yes":
+ [(1, "2005 Teacher Percentage of School Staff"),
+ (2, "2010 Teacher Percentage of School Staff"),
+ (6, "2015 Teacher Percentage of School Staff"),
+ (15, "2018 Teacher Percentage of School Staff")],
+
+"https://nces.ed.gov/programs/digest/d19/tables/dt19_214.30.asp?current=yes":
+[(1, "2018 Number of Education Agencies")]}
+
+data2 = {"https://nces.ed.gov/programs/digest/d19/tables/dt19_219.35.asp?current=yes":
+(True, (8, 15), 0, "Average Freshman Graduation Rate"),
+
+
+
+"https://nces.ed.gov/programs/digest/d19/tables/dt19_221.40.asp?current=yes":
+(True, (5, 13), 1, "4th Grade Reading Scores"),
+
+"https://nces.ed.gov/programs/digest/d19/tables/dt19_222.60.asp?current=yes":
+(True, (5, 13), 1, "8th Grade Math Scores"),
+
+
+"https://nces.ed.gov/programs/digest/d19/tables/dt19_222.50.asp?current=yes": 
+(True, (4, 12), 1, "4th Grade Math Scores"),
+
+ "https://nces.ed.gov/programs/digest/d19/tables/dt19_211.60.asp?current=yes":
+ (False, (8, 14), 1, "Overall Average Teacher Salary"),
+
+ "https://nces.ed.gov/programs/digest/d20/tables/dt20_216.90.asp?current=yes":
+(False, (10, 15), 1, "% of Public Schools That Are Charters"),
+
+"https://nces.ed.gov/programs/digest/d19/tables/dt19_219.46.asp?current=yes":
+(True, (0, 8), 1, "Adjusted Cohort Graduation Rate")}
 
 
 not_states = ['Other jurisdictions', 'American Samoa', 'Guam',
@@ -30,14 +95,6 @@ def set_index(df):
     df = df.set_index(top_left)
     return df
     
-#3 
-def remove_states(df):
-    for index in df.index:
-        if index in not_states:
-            df = df.drop(index, axis=0)
-    
-    return df
-
 
 
 
@@ -52,10 +109,14 @@ def remove_footnotes(df):
 
 
 #5
-def drop_se1(df):
-    df.columns = df.columns.droplevel()
-    return df
-     
+def drop_se(df):
+    cols = df.columns
+    index = len(cols[0]) - 1
+    drops = []
+    for col in cols:
+        if "." in col[index]:
+            drops.append(col)
+    df.drop(columns=drops, inplace=True)
     
 #6    
 def drop_se2(df):
@@ -88,107 +149,144 @@ def remove_repeat(df):
     new = df[[df.columns[0]]].copy()
     return new
 
+def replace_na(df):
+    df = df.replace({'‡': None, "#": None, "—": None, "---": None})
+    df = df.fillna(df.iloc[0])
+    return df
 
-
-    
-
-
-
-data = {"https://nces.ed.gov/programs/digest/d19/tables/dt19_203.90.asp?current=yes" :
-[('[Standard errors appear in parentheses]',
- '2011-12',
- 'Total elementary, secondary, and combined elementary/secondary schools',
- 'ADA as percent of enrollment','4'), "Average Daily Attendance %"],
-
-"https://nces.ed.gov/programs/digest/d19/tables/dt19_204.75c.asp?current=yes":
-[('Homeless students as percent of total public school enrollment',
- '2016-17',
- '17'), "Percent of Students Experiencing Homelessness"],
-
-"https://nces.ed.gov/programs/digest/d19/tables/dt19_204.90.asp?current=yes":
-[('[Standard errors appear in parentheses]', '2013-141', 'Total', 'Total', 'Total', '5', '6.7'),
-"Students Enrolled in Gifted Programs %"],
-
-"https://nces.ed.gov/programs/digest/d19/tables/dt19_219.35.asp?current=yes":
-[('2012-13', '16'), "Average Freshman Graduation Rate"],
-
-"https://nces.ed.gov/programs/digest/d19/tables/dt19_221.40.asp?current=yes":
-[('[Standard errors appear in parentheses]', '2019', '14'), "4th Grade Reading Scores"],
-
-
-"https://nces.ed.gov/programs/digest/d19/tables/dt19_219.85b.asp?current=yes": 
-[('[Standard errors appear in parentheses]', 'Total', '2'), 
-"Percentage of HS Drop Outs Age 16-24"],
-
-"https://nces.ed.gov/programs/digest/d19/tables/dt19_222.60.asp?current=yes":
-[('[Standard errors appear in parentheses]', '2019', '14'), "8th Grade Math Scores"],
-
-"https://nces.ed.gov/programs/digest/d19/tables/dt19_221.72.asp?current=yes":
-[('[Standard errors appear in parentheses]', '8th-grade', 'Total', 'Total', '7'),
-"8th Grade Reading Scores"],
-
-"https://nces.ed.gov/programs/digest/d19/tables/dt19_222.50.asp?current=yes": 
-[('[Standard errors appear in parentheses]', '2019', '13'),
-"4th Grade Math Scores"], 
-
-"https://nces.ed.gov/programs/digest/d19/tables/dt19_211.30.asp?current=yes":
-[('[Standard errors appear in parentheses]', 'Current dollars', '2017-18',
- 'Total', 'Total', '9'), "Average Base Teacher Salary w/ Bachelors"],
-
-"https://nces.ed.gov/programs/digest/d19/tables/dt19_211.40.asp?current=yes":
-[('[Standard errors appear in parentheses]', 'Current dollars', '2017-18',
- 'Total', 'Total', '10'), "Average Base Teacher Salary w/ Masters"],
-
- "https://nces.ed.gov/programs/digest/d19/tables/dt19_211.60.asp?current=yes":
- [('Constant 2018-19 dollars1', 'Percentchange,1999-2000to2018-19', '16'), 
- "% Change in Teacher Salary 1999-2019"],
-
- "https://nces.ed.gov/programs/digest/d19/tables/dt19_211.60.asp?current=yes":
- [('Constant 2018-19 dollars1', '2018-19', '15'), "Overall Average Teacher Salary"],
-
- "https://nces.ed.gov/programs/digest/d20/tables/dt20_213.40.asp?current=yes":
- [('[In full-time equivalents]', 'Fall 2018', 'Teachers as a percent of staff',
- 'Teachers as a percent of staff', 'Teachers as a percent of staff',
- 'Teachers as a percent of staff', '17'), "Teacher Percentage of School Staff"],
-
-"https://nces.ed.gov/programs/digest/d19/tables/dt19_214.30.asp?current=yes":
-[('Total agencies', 'Total agencies', '2017-18', '3'), "Number of Education Agencies"],
-
-"https://nces.ed.gov/programs/digest/d20/tables/dt20_216.90.asp?current=yes":
-[('Charter schools as a percent of total public schools', '2017-18', '15'),
-"% of Public Schools That Are Charters"],
-
-"https://nces.ed.gov/programs/digest/d19/tables/dt19_219.46.asp?current=yes":
-[('Total, ACGR for all students', '2017-18', '2017-18', '2017-18', '9'), 
-"Adjusted Cohort Graduation Rate"]}
-
-dfs = []
-for thing1, thing2 in data.items():
-    df = grab_frame(thing1)
-    df = set_index(df)
-    df = remove_footnotes(df)
-    df = df[thing2[0]].to_frame()
-    df.columns = [thing2[1]]
-    dfs.append(df)
-
-first_frame = dfs[0]
-for i in range(1, len(dfs)):
-    if i == 1:
-        final = first_frame.join(dfs[i])
-    else:
-        final = final.join(dfs[i])
 
 def remove_dollar(df):
-    dol_cols = ['Average Base Teacher Salary w/ Bachelors',
-    'Average Base Teacher Salary w/ Masters',
-    'Overall Average Teacher Salary']
-
-    for col in dol_cols:
+    for col in df.columns:
+        df[col] = df[col].astype(str)
         df[col] = df[col].str.replace(r"[$,]", '')
     return df
 
-final = remove_dollar(final)
+def df_crawl1(df_dict):
+    dfs = []
+    for key, value in df_dict.items():
+        df = grab_frame(key)
+        df = set_index(df)
+        df = remove_footnotes(df)
+        if len(value) == 1:
+            df = df[df.columns[value[0][0]]].to_frame()
+            df.columns = [value[0][1]]
+            df = replace_na(df)
+            dfs.append(df)
+        else:
+            frames = []
+            for i in range(len(value)):
+                frame = df[df.columns[value[i][0]]].to_frame()
+                frame.columns = [value[i][1]]
+                frame = replace_na(frame)
+                frames.append(frame)
+            first_frame = frames[0]
+            for i in range(1, len(frames)):
+                if i == 1:
+                    dataframe = first_frame.join(frames[i])
+                else:
+                    dataframe = dataframe.join(frames[i])
+            dfs.append(dataframe)
+    return dfs
 
+
+
+
+def df_crawl2(df_dict):
+    dfs = []
+    for key, value in df_dict.items():
+        df = grab_frame(key)
+        df = set_index(df)
+        df = remove_footnotes(df)
+        if value[0] == True:
+            drop_se(df)
+        cols = []
+        for i in range(value[1][0], value[1][1]):
+            col = df[df.columns[i]].to_frame()
+            col.columns = [df.columns[i][value[2]][:4] + " " + value[3]] 
+            col = replace_na(col)
+            cols.append(col)
+        first_col = cols[0]
+        for i in range(1, len(cols)):
+            if i == 1:
+                dataframe = first_col.join(cols[i])
+            else:
+                dataframe = dataframe.join(cols[i])
+        dfs.append(dataframe)
+    return dfs
+
+
+
+
+
+
+
+dfs1 = df_crawl1(data)
+dfs1[4] = remove_dollar(dfs1[4])
+dfs1[5] = remove_dollar(dfs1[5])
+dfs2 = df_crawl2(data2)
+dfs2[4] = remove_dollar(dfs2[4])
+
+
+def raw_data(dfs):
+    first_frame = dfs[0]
+    for i in range(1, len(dfs)):
+        if i == 1:
+            raw = first_frame.join(dfs[i])
+        else:
+            raw.join(dfs[i])
+    return raw
+
+raw = raw_data(dfs1).join(raw_data(dfs2))
+
+
+
+def normalize(df):
+    x = df.values
+    min_max_scaler = preprocessing.MinMaxScaler()
+    x_scaled = min_max_scaler.fit_transform(x)
+    normalized = pd.DataFrame(x_scaled)
+    normalized.index = df.index
+    normalized.columns = df.columns
+    for col in normalized.columns:
+        normalized[col] = 100* normalized[col]
+    return normalized
+
+
+def get_slope(row):
+    axisvalues = list(range(1, len(row) + 1))
+    regression = scipy.stats.linregress(row, y=axisvalues)
+    return regression.slope 
+
+
+def add_slope(df):
+    col_name = "Trend: " + df.columns[0][5:]
+    df[col_name] = df.apply(get_slope, axis=1)
+    return df
+
+
+def final_frame(dfs):
+    final_dfs = []
+    for df in dfs:
+        if len(df.columns) == 1:
+            df = normalize(df)
+            final_dfs.append(df)
+        else:
+            df = normalize(df)
+            df = add_slope(df)
+            final_dfs.append(df)
+    first_frame = final_dfs[0]
+    for i in range(1, len(final_dfs)):
+        if i == 1:
+            final = first_frame.join(final_dfs[i])
+        else:
+            final = final.join(final_dfs[i])
+    return final
+
+
+
+final = final_frame(dfs1).join(final_frame(dfs2))
+
+final["2018 Percentage of HS Drop Outs Age 16-24"] = 100 - final["2018 Percentage of HS Drop Outs Age 16-24"]
 
 us_state_abbrev = {
     'Alabama': 'AL',
@@ -250,31 +348,55 @@ us_state_abbrev = {
     'United States': 'US'
 }
 final.rename(index=us_state_abbrev, inplace=True)
+final.fillna(final.mean(), inplace=True)
 
 
-
-def replace_na(df):
-    df = df.fillna(0)
-    df = df.replace({'‡': 0, "#": 0})
-    for col in df.columns:
-        df[col].replace({0: df[col][0]}, inplace=True)
-    return df
+# outfile = "/home/jrgill/Desktop/cmsc12200-win-21-jrgill/Project/place"
+# y = "final.csv"
 
 
-
-
-filled = replace_na(final)
+# final.to_csv(os.path.join(outfile, y))
 
 
 
 
-def normalize(df):
-    x = df.values
-    min_max_scaler = preprocessing.MinMaxScaler()
-    x_scaled = min_max_scaler.fit_transform(x)
-    df = pd.DataFrame(x_scaled)
-    return df
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# dfs = []
+#     df = grab_frame(thing1)
+#     df = set_index(df)
+#     df = remove_footnotes(df)
+#     df = df[thing2[0]].to_frame()
+#     df.columns = [thing2[1]]
+#     dfs.append(df)
+
+# first_frame = dfs[0]
+# for i in range(1, len(dfs)):
+#     if i == 1:
+#         final = first_frame.join(dfs[i])
+#     else:
+#         final = final.join(dfs[i])
 
