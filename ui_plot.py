c9c352a7ac444a59d542ed201d52f666afa887b2
tmlaurencio <t.m.laurencio@gmail.com>
2021-03-11 19:56:26 -0600

    changed inputs to functions

--- ui_plot.py
+++ ui_plot.py
@@ -5,15 +5,13 @@ import pandas as pd
 import program_hq as phq
 data = pd.read_csv('final.csv')
 
-def scplot(dat, outcome, linmod):
+def scplot(dat, linmod):
     """
     Plots scatter plot with regression line, visualization of dataframes for base data and 
     regression results, and the correlation matrix dataframe
 
     Inputs:
-    dat (Pandas DataFrame): The policy data (independent variables)
-    outcome (Pandas Series): The outcome; must have 
-                             same number of rows as dat (dependent variable)
+    dat (Pandas DataFrame): A df with two columns, outcome and best policy
     linmod (dict): a dictionary mapping the policy name to the corresponding coefficients
                    in the linear model, as well as the intercept and model r^2 score (output
                    from fws)
@@ -21,37 +19,24 @@ def scplot(dat, outcome, linmod):
     Outputs:
     (Figure) The plotted model
     """
-    #could probably do this operation with DataFrame.apply but unsure
-    temp = dat.copy()
-    for col in dat.columns:
-        if col in linmod.keys(): #assumes no duplicates in column names
-            temp[col] = temp[col].apply(lambda x: x * linmod[col])
+    #series containing predicted values for outcome
+    y_pred = dat.iloc[:,1].apply(lambda x: x * linmod[dat.columns[1]])
     
-    y_pred = temp.sum(axis = 1) #series containing predicted values for outcome
-    
-    #this is the variable with highest coefficient but
-    #should change to variable with highest score from bivariate model
-    strong = max(linmod, key = linmod.get) 
-    
-
     #scatterplot with regression line
-    plt.plot(dat[strong], y_pred)
-    plt.scatter(dat[strong], outcome)
-    plt.xlabel(strong)
-    plt.ylabel(outcome.name)
-    plt.title("{a} vs. {b}".format(a = strong, b = outcome.name))
+    plt.plot(dat.iloc[:,1], y_pred)
+    plt.scatter(dat.iloc[:,1], dat.iloc[:,0])
+    plt.xlabel(dat.columns[1])
+    plt.ylabel(dat.columns[0])
+    plt.title("{a} vs. {b}".format(a = dat.columns[0], b = dat.columns[1]))
 
 
-
-def dat_df(dat, outcome, linmod):
+def dat_df(dat, linmod):
     """
     Prints visualization of dataframes for base data and 
     regression results, and the correlation matrix dataframe
 
     Inputs:
-    dat (Pandas DataFrame): The policy data (independent variables)
-    outcome (Pandas Series): The outcome; must have 
-                             same number of rows as dat (dependent variable)
+    dat (Pandas DataFrame): A df with two columns, outcome and best policy
     linmod (dict): a dictionary mapping the policy name to the corresponding coefficients
                    in the linear model, as well as the intercept and model r^2 score (output
                    from fws)
@@ -61,23 +46,17 @@ def dat_df(dat, outcome, linmod):
           correlation matrix dataframe 
     """
     #could probably do this operation with DataFrame.apply but unsure
-    temp = dat.copy()
-    for col in dat.columns:
-        if col in linmod.keys(): #assumes no duplicates in column names
-            temp[col] = temp[col].apply(lambda x: x * linmod[col])
-    
-    y_pred = temp.sum(axis = 1) #series containing predicted values for outcome
+    y_pred = dat.iloc[:,1].apply(lambda x: x * linmod[dat.columns[1]])
     
     #source_df contains base data, actual outcome values, and predicted outcome
-    #values from the regression.  The first column from the right contains the 
-    #outcome data while the rightmost column contains the regression predictions 
-    #for the outcome data
-    source_dfs = dat.insert(len(dat.columns), outcome.name, outcome)
-    source_dfs = source_dfs.insert(len(source_dfs.columns), outcome.name + ' pred', y_pred)
-    print(source_dfs)
+    #values from the regression.  Columns are as follows: outcome, predicted 
+    #outcome (from regression), and best policy
+    source_df = dat.insert(1, dat.columns[0] + " pred", y_pred)
+    print(source_df)
 
     #visualization of regression coefficients: since each value i is a scalar, 
     #need to convert values to arraylike to convert to dataframe
+    #may have to limit this to only the best policy rather than all policy inputs
     lst_dict = {k: [i] for k, i in linmod.items()}
     print(pd.DataFrame.from_dict(lst_dict))
 
