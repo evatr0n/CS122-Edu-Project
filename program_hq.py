16d47d95ceff4a6b088ebcdca02d85fff057b5ca
Jake Underland <jakez0626@gmail.com>
2021-03-13 08:39:50 +0900

    how to draw data from csv

--- program_hq.py
+++ program_hq.py
@@ -25,14 +25,21 @@ states_overall_effectiveness_score, state_to_policy_effectiveness_score, policy_
     d.default_calc(avg_nctq, centered_avg_nctq, nces_trends, 0.05)
 print("Loading completed")
 
+#################################################################################
+# get dataframes directly from csv files instead of running crawlers
+
 
 nctqdic_original = {}
 for filename in os.listdir("csv/"):
     if filename.startswith("nctq"):
         nctqdic_original[filename.strip(".csv")] = pd.read_csv("csv/{}".format(filename), index_col = 0)
+nctqdic_filled = nctq.fill_na(nctqdic_original)
+avg_nctq = nctq.average_df(nctqdic_filled).sort_index()  
+centered_avg_nctq = nctq.center_df(avg_nctq)
 
 nces_final = pd.read_csv("csv/nces_final.csv", index_col=0)
 nces_trends = nces_final[[col for col in nces_final.columns if col.startswith("Trend")]]
+nces_original = pd.read_csv("csv/nces_raw.csv", index_col=0)
 '''
     try:
         year = int(filename.strip(".csv")[-4:])
