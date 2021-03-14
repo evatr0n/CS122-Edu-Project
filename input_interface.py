fe64f12a64488d613ac83e2563f3a97a5194f987
evatr0n <63478627+evatr0n@users.noreply.github.com>
2021-03-14 13:36:19 -0500

    working, all appropriate comments and formatting

--- input_interface.py
+++ input_interface.py
@@ -1,11 +1,10 @@
-# tkinter_practice.py
+# main function: run this to run our software!
 
 import tkinter as tk
 from tkinter import ttk
 import pandas as pd
 import os
 
-#import nces
 import nctq
 import output1
 import output2
@@ -13,8 +12,6 @@ import default_stat_analysis as dsa
 import basic_regression as b
 from ui_util import VerticalScrolledFrame
 from ui_util import NewWindow
-
-
 
 # Import the nces data
 nces_final = pd.read_csv("csv/nces_final.csv", index_col = 0)
@@ -31,11 +28,10 @@ avg_nctq = nctq.average_df(nctqdic_filled).sort_index()
 centered_avg_nctq = nctq.center_df(avg_nctq)
 
 # Run function to get scores for Option 1 (so this never needs to be run again)
-R2 = 0.1 # change this in function call as needed, this is just a placeholder
+R2 = 0.1 
 states_score_dict, state_to_pols_score_dict= dsa.default_calc(
         avg_nctq, centered_avg_nctq, nces_trends, R2, block_negative=True, outcomes=None
     )
-
 
 states = ["US", "AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DC", "DE", "FL", "GA", 
           "HI", "ID", "IL", "IN", "IA", "KS", "KY", "LA", "ME", "MD", 
@@ -48,6 +44,9 @@ trend_outcomes = list(nces_trends.columns)
 
 class Window1:
     def __init__(self, root):
+        """
+        Constructor for the window class that takes in a tkinter root object
+        """
         self.screen_width = root.winfo_screenwidth()
         self.screen_height= root.winfo_screenheight()
         self.frame = VerticalScrolledFrame(
@@ -62,13 +61,15 @@ class Window1:
         self.default_opt_frame()
         self.fws_nondefault_frame()
         self.special_opt_frame()
-        #self.scrollbar()
         self.r2_cutoff_frame()
         self.bottom_frame()
     
+
     def welcome_frame(self):
+        """
+        Function that creates the topmost frame in the UI, which includes a short description of our tool
+        """
         welcome_frame = tk.Frame(self.frame, bg = "pink")
-        #welcome_frame.grid(column = 0, row = 0)
         welcome_frame.pack(expand=True, fill='x')
         welcome_text_box = tk.Text(welcome_frame, height = 12, bg = "pink", bd = 0, relief = tk.FLAT, wrap = tk.WORD)
         welcome_text_box.grid(column = 0, row = 0)
@@ -78,19 +79,23 @@ class Window1:
         welcome_text_box.insert(tk.END, intro + "\n"*2 + sources + "\n"*2 + note)
         welcome_text_box.configure(state='disabled')
     
+
     def default_opt_frame(self):
+        """
+        Function that creates the frame that holds the first user input option, which allows for state seletion
+        """
         default_opt_frame = tk.Frame(self.frame, bg = "dark blue")
         default_opt_frame.pack(expand=True, fill='x')
-        #grid(column = 0, row = 1)
+
         option1_label = ttk.Label(default_opt_frame, text = "Option 1 Retrieve information on how effective a state's relevant policies are in relation to all available educational outcomes: ").grid(column = 0,  
         row = 0, padx = 35, pady = 25, columnspan = 3)
-        # State Selection Label
         state_label = ttk.Label(default_opt_frame, text = "Select one state, or select multiple to compare").grid(column = 0,  
                 row = 1, padx = 35, pady = 25, columnspan=2) 
 
         # State Selection listbox widget
         self.state_listbox = tk.Listbox(default_opt_frame, selectmode = "multiple", exportselection = False, width=20, height=10)
         self.state_listbox.grid(column = 0, row = 2, sticky="E", pady = 25)
+
         # Adding scrollbar to listbox
         state_scrollbar = tk.Scrollbar(default_opt_frame)
         state_scrollbar.config(command=self.state_listbox.yview)
@@ -102,17 +107,18 @@ class Window1:
     
             self.state_listbox.insert(tk.END, state) 
             self.state_listbox.itemconfig(i, bg = "deep sky blue")
-        
+
         # Calculate Button
         button = tk.Button(default_opt_frame, text="Calculate!", bd = "5", command = lambda: self.retrieve1(states_score_dict, state_to_pols_score_dict))
-        #command=self.retrieve1)
         button.grid(column = 2, row = 2)
     
     def fws_nondefault_frame(self):
+        """
+        Function that creates the frame that holds the second user input option, which allows for state and outcome variable seletion
+        """
         fws_nondefault_frame = tk.Frame(self.frame, bg = "purple")
         fws_nondefault_frame.pack(expand=True, fill='x')
-        #grid(column = 0, row = 2)
-        # Label 
+
         option2_label = ttk.Label(fws_nondefault_frame, text = "Option 2: Recalculate state scores by specifying particular outcome variables").grid(column = 0,  
         row = 0, padx = 35, pady = 25, columnspan = 2, sticky="W")
         state_label = ttk.Label(fws_nondefault_frame, text = "Select one state, or select multiple to compare").grid(column = 0,  
@@ -121,13 +127,14 @@ class Window1:
         # List box widget
         self.state2_listbox = tk.Listbox(fws_nondefault_frame, selectmode = "multiple", exportselection = False, width=20, height=10)
         self.state2_listbox.grid(column = 0, row = 2, sticky="E")
-        # Adding scrollbar to listbox
+
+        # Adding scrollbar to states listbox
         state_scrollbar = tk.Scrollbar(fws_nondefault_frame)
         state_scrollbar.config(command=self.state2_listbox.yview)
         self.state2_listbox.config(yscrollcommand=state_scrollbar.set)
         state_scrollbar.grid(column=1, row=2, sticky='NSW')
 
-        # Populating scrollbar
+        # Populating states listbox
         for i, state in enumerate(states): 
     
             self.state2_listbox.insert(tk.END, state) 
@@ -136,14 +143,15 @@ class Window1:
         ## Outcomes selection listbox widget
         outcomes2_label = ttk.Label(fws_nondefault_frame, text = "Select the outcomes you want to investigate:").grid(column = 2,  
         row = 1, padx = 35, pady = 25, columnspan=2) 
-        # outcomes Selection listbox widget
         self.outcomes2_listbox = tk.Listbox(fws_nondefault_frame, selectmode = "multiple", exportselection = False, width=35, height=10)
         self.outcomes2_listbox.grid(column = 2, row = 2, sticky="E")
+
         # Adding vertical scrollbar to outcomes listbox
         outcomes2_scrollbar = tk.Scrollbar(fws_nondefault_frame)
         outcomes2_scrollbar.config(command=self.outcomes2_listbox.yview)
         self.outcomes2_listbox.config(yscrollcommand=outcomes2_scrollbar.set)
         outcomes2_scrollbar.grid(column=3, row=2, sticky='NSW')
+
         # Adding horizontal scrollbar to outcomes listbox
         outcomes2_scrollbarh = tk.Scrollbar(fws_nondefault_frame, orient="horizontal")
         outcomes2_scrollbarh.config(command=self.outcomes2_listbox.xview)
@@ -154,36 +162,40 @@ class Window1:
         button = tk.Button(fws_nondefault_frame, text="Calculate!", bd = "5", command=self.retrieve2)
         button.grid(column = 4, row = 2, padx = 35, pady = 25)
 
+        # Populate the outcomes listbox
         for i, outcome in enumerate(trend_outcomes): 
     
             self.outcomes2_listbox.insert(tk.END, outcome) 
             self.outcomes2_listbox.itemconfig(i, bg = "deep sky blue")
     
+
     def special_opt_frame(self):
+        """
+        Function that creates frame and holds user input option 3, which allows for an outcome variable
+        choice and chosing multiple policies
+        """
         special_opt_frame = tk.Frame(self.frame, bg = "green")
         special_opt_frame.pack(expand=True, fill='x')
-        #grid(column = 0, row = 3)
-        # Label 
+
         option3_label = ttk.Label(special_opt_frame, text = "Option 3: Retrieve information on how particular policies interact with a given outcome: ").grid(column = 0,  
         row = 0, padx = 35, pady = 25)
-
         ttk.Label(special_opt_frame, text = "Select one outcome to investigate:").grid(column = 0,  
                 row = 1, padx = 35, pady = 25)
+
+        # Combobox widget for outcome choices        
         outcome_combo = tk.StringVar() 
         self.outcomes_combobox = ttk.Combobox(special_opt_frame, width = 27,  
                                     textvariable = outcome_combo, exportselection=0)
         self.outcomes_combobox['values'] = outcomes 
         self.outcomes_combobox.grid(column = 0, row = 2) 
 
-        # policies selection label
+        # Policies selection listbox widget
         policies_label = ttk.Label(special_opt_frame, text = "Select which policies to consider:").grid(column = 1,  
                 row = 1, padx = 35, pady = 25, columnspan = 2) 
-
-        # policies Selection listbox widget
         self.policies3_listbox = tk.Listbox(special_opt_frame, selectmode = "multiple", exportselection = False, width=35, height=10)
         self.policies3_listbox.grid(column = 1, row = 2, sticky="E")
 
-        # Adding scrollbar to listbox
+        # Adding veritcal scrollbar to policies listbox
         policies3_scrollbar = tk.Scrollbar(special_opt_frame)
         policies3_scrollbar.config(command=self.policies3_listbox.yview)
         self.policies3_listbox.config(yscrollcommand=policies3_scrollbar.set)
@@ -199,21 +211,27 @@ class Window1:
         button = tk.Button(special_opt_frame, text="Calculate!", bd = "5", command=self.retrieve3)
         button.grid(column = 4, row = 2, padx = 35, pady = 25)
 
+        # Populate the policies listbox
         for i, policy in enumerate(avg_nctq.columns): 
       
             self.policies3_listbox.insert(tk.END, policy) 
             self.policies3_listbox.itemconfig(i, bg = "deep sky blue") 
         
+
     def r2_cutoff_frame(self):
+        """
+        Function that creates and holds the frame for the helper tool, which allows a user to pick an outcome and R2 value
+        and prints in a textbox the policies that have a correlation higher than R2 with that chosen outcome variable
+        """
         r2_frame = tk.Frame(self.frame, bg = "orange")
         r2_frame.pack(expand=True, fill='x')
-        #grid(column = 0, row = 3)
-        # Label 
+
         r2_label = ttk.Label(r2_frame, text = "Helper tool: Given a particular outcome, find all policies with a correlation greater than a given R2").grid(column = 0,  
         row = 0, padx = 35, pady = 25, columnspan = 2, sticky = "w")
-
         ttk.Label(r2_frame, text = "Select one outcome to investigate:").grid(column = 0,  
                 row = 1, padx = 35, pady = 25)
+
+        # Outcomes combobox widget
         outcome_combo = tk.StringVar() 
         self.r2outcomes_combobox = ttk.Combobox(r2_frame, width = 27,  
                                     textvariable = outcome_combo, exportselection=0)
@@ -223,8 +241,6 @@ class Window1:
         # r2 value entry box
         r2_label = ttk.Label(r2_frame, text = "Enter an R2 value (number):").grid(column = 1,  
                 row = 1, padx = 35, pady = 25, columnspan = 2) 
-
-        # policies Selection listbox widget
         self.entry = tk.StringVar()
         r2_entry = tk.Entry(r2_frame, exportselection = False, width=5, relief=tk.SUNKEN, textvariable = self.entry)
         r2_entry.grid(column = 1, row = 2)
@@ -239,34 +255,69 @@ class Window1:
         self.r2_textbox.config(yscrollcommand=textbox_scrollbar.set)
         textbox_scrollbar.grid(column=2, row=3, pady = 25, sticky='NSW')
 
-
-         # Adding calculate button widget 
+        # Adding calculate button widget 
         button = tk.Button(r2_frame, text="Submit", bd = "5", command=self.retrieve4)
         button.grid(column = 4, row = 2, padx= 35, pady = 25)
-        
+
+
+    def bottom_frame(self):
+        """
+        This function creates the final frame with holds buttons that give the user options to view our data
+        we are using for calculations, as well as get definitions for the policy names
+        """
+        bottom_frame = tk.Frame(self.frame, bg = "gold2")
+        bottom_frame.pack(expand=True, fill='x', pady = (0, 70))
+        # NCES button
+        button1 = tk.Button(bottom_frame, text="Click here to see the NCES education outcome data we utilized in our calculations", pady=20, wraplength=250, width = 40)
+        button1.grid(column = 0, row = 0, padx = 30, pady = 20)
+        button1.bind("<Button>", lambda e: NewWindow("csv/nces_final.csv", self.frame))
+        # NCTQ button
+        button2 = tk.Button(bottom_frame, text="Click here to see the NCTQ policy grades we utilized in our calculations", pady=20, wraplength=250, width = 40)
+        button2.grid(column = 1, row = 0, padx = 30, pady = 20)
+        button2.bind("<Button>", lambda e: NewWindow("csv/avg_nctq.csv", self.frame))
+        # Policy descriptions button
+        button3 = tk.Button(bottom_frame, text="Click here to see the policy descriptions according to the NCTQ", pady=20, wraplength=250, width = 40)
+        button3.grid(column = 2, row = 0, padx = 30, pady = 20)
+        button3.bind("<Button>", lambda e: NewWindow("csv/policydesc_dic.json", self.frame))
+
 
     def scrollbar(self):
+        """
+        Implements a scrollbar
+        """
         scrollbar = tk.Scrollbar(self.frame)
         return scrollbar
-        #scrollbar.pack(side="right", fill="y")
+
     
     def retrieve1(self, states_score_dict, state_to_pols_score_dict):
+        """
+        Function called when user hits the button for the 1st input option.
+        It creates a dictionary with each state = key, and value = get_score() return values
+        From default_stat_analysis.py. These return values have the state's score, best and worst
+        policies for that particular state. These are passed into a new output window.
+        Inputs: states_score_dict: dict that maps states to their effectiveness scores,
+                    is the return from default_calc as a global variable
+                state_to_pols_score_dict: dict that maps each state to a another dictionary of
+                    policies, where each key = policy name, and effectiveness score for that policy
+                    on that state's outcome
+        """
         states = [self.state_listbox.get(idx) for idx in self.state_listbox.curselection()]
         outcomes = list(nces_trends.columns)
         return_dict = {}
         for state in states:
             score, best_pol, worst_pol = dsa.get_scores(states_score_dict, state_to_pols_score_dict, state)
             return_dict[state] = [score, best_pol, worst_pol]
-        print("states" + str(states))
-        # for testing purposes
-        """
-        return_dict = {"IL": [50, "Pay Scales (Retaining Effective Teachers Policy)", "	Academic Requirements (Early Childhood Preparation Policy)"], 
-        "CA": [75, "Pension Flexibility (Retaining Effective Teachers Policy)", "Induction (Retaining Effective Teachers Policy)"]}
-        """
         self.new_window1(return_dict, outcomes)
 
 
     def retrieve2(self):
+        """
+        Function called when user hits the button for the 2st input option. It runs the default_calc()
+        once again given a different set of outcomes than the default of all outcomes.
+        It creates a dictionary with each state = key, and value = get_score() return values
+        From default_stat_analysis.py. These return values have the state's score, best and worst
+        policies for that particular state. These are passed into a new output window.
+        """
         states = [self.state2_listbox.get(idx) for idx in self.state2_listbox.curselection()]
         outcomes = [self.outcomes2_listbox.get(idx) for idx in self.outcomes2_listbox.curselection()]
         states_score_dict, state_to_pols_score_dict = dsa.default_calc(
@@ -276,24 +327,24 @@ class Window1:
         for state in states:
             score, best_pol, worst_pol = dsa.get_scores(states_score_dict, state_to_pols_score_dict, state)
             return_dict[state] = [score, best_pol, worst_pol]
-        print("states" + str(states))
-        print("outcomes" + str(outcomes))
-        # for testing purposes
-        """
-        return_dict = {"IL": [50, "Pay Scales (Retaining Effective Teachers Policy)", "	Academic Requirements (Early Childhood Preparation Policy)"], 
-        "CA": [75, "Pension Flexibility (Retaining Effective Teachers Policy)", "Induction (Retaining Effective Teachers Policy)"]}
-        """
         self.new_window1(return_dict, outcomes)
 
 
     def retrieve3(self):
+        """
+        Function called when user hits 3rd input option button, and passes those selected inputs
+        into a new output window.
+        """
         outcome = self.outcomes_combobox.get()
         policies = [self.policies3_listbox.get(idx) for idx in self.policies3_listbox.curselection()]
-        print("outcome: " + str(outcome))
-        print("policies" + str(policies))
         self.new_window2(outcome, policies)
 
+
     def retrieve4(self):
+        """
+        Function called when user hits submit button in the 4th helper tool frame
+        Returns the appropriate policies to the text widget.
+        """
         outcome = self.r2outcomes_combobox.get()
         self.r2_textbox.delete(1.0, tk.END)
         try:
@@ -310,38 +361,34 @@ class Window1:
         
 
     def new_window1(self, return_dict, outcomes):
+        """
+        Creates a new output window for user input option 1 and 2
+        """
         self.newWindow1 = tk.Toplevel(self.frame)
         self.output1 = output1.Output1(self.newWindow1, return_dict, outcomes)
 
 
     def new_window2(self, outcome, policies):
+        """
+        Creates a new output window for user input option 3
+        """
         self.newWindow2 = tk.Toplevel(self.frame)
         self.output2 = output2.Output2(self.newWindow2, nces_final, avg_nctq, outcome, policies)
 
 
-    def bottom_frame(self):
-        bottom_frame = tk.Frame(self.frame, bg = "gold2")
-        bottom_frame.pack(expand=True, fill='x', pady = (0, 70))
-        button1 = tk.Button(bottom_frame, text="Click here to see the NCES education outcome data we utilized in our calculations", pady=20, wraplength=250, width = 40)
-        button1.grid(column = 0, row = 0, padx = 30, pady = 20)
-        button1.bind("<Button>", lambda e: NewWindow("csv/nces_final.csv", self.frame))
-        button2 = tk.Button(bottom_frame, text="Click here to see the NCTQ policy grades we utilized in our calculations", pady=20, wraplength=250, width = 40)
-        button2.grid(column = 1, row = 0, padx = 30, pady = 20)
-        button2.bind("<Button>", lambda e: NewWindow("csv/avg_nctq.csv", self.frame))
-        button3 = tk.Button(bottom_frame, text="Click here to see the policy descriptions according to the NCTQ", pady=20, wraplength=250, width = 40)
-        button3.grid(column = 2, row = 0, padx = 30, pady = 20)
-        button3.bind("<Button>", lambda e: NewWindow("csv/policydesc_dic.json", self.frame))
-
-
-def main(): #run mianloop 
+def main(): 
+    """
+    This function runs everything in this file, and makes calls to all other necessary files.
+    """
     root = tk.Tk()
     root["bg"] = "white"
-    #getting screen width and height of display 
+    # Getting screen width and height of display 
     screen_width = root.winfo_screenwidth()  #in pixels
     screen_height= root.winfo_screenheight() #in pixels
-    #setting tkinter window size 
+    # Setting tkinter window size 
     root.geometry("%dx%d" % (screen_width, screen_height))
     root.title("CS122 TJJE Project: Examining State Policies and Educational Outcomes")
+    # Applying class
     app = Window1(root)
     root.mainloop()
 
