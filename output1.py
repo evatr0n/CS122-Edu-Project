e81b4408724656e64de2937b3b34f0bb3dd05a05
Jake Underland <jakez0626@gmail.com>
2021-03-15 02:34:25 +0900

    output has scrolls

--- output1.py
+++ output1.py
@@ -28,13 +28,13 @@ nctqdic_filled = nctq.fill_na(nctqdic_original)
 avg_nctq = nctq.average_df(nctqdic_filled).sort_index()  
 centered_avg_nctq = nctq.center_df(avg_nctq)
 
-"""
+
 return_dict = {"IL": [50, "Pay Scales (Retaining Effective Teachers Policy)", "Academic Requirements (Early Childhood Preparation Policy)"], 
         "CA": [75, "Pension Flexibility (Retaining Effective Teachers Policy)", "Induction (Retaining Effective Teachers Policy)"],
         "NY": [90, "Pension Flexibility (Retaining Effective Teachers Policy)", "Induction (Retaining Effective Teachers Policy)"]}
 outcomes = ['Trend: Average Daily Attendance %', 'Trend: Students Enrolled in Gifted Programs %', 'Trend: Average Base Teacher Salary w/ Bachelors']
 #'Trend: Average Base Teacher Salary w/ Masters Constant Dollars', 'Trend: Teacher Percentage of School Staff', 'Trend: Average Freshman Graduation Rate', 'Trend: 4th Grade Reading Scores', 'Trend: 8th Grade Math Scores', 'Trend: 4th Grade Math Scores', 'Trend: Overall Average Teacher Salary', 'Trend: % of Public Schools That Are Charters', 'Trend: Adjusted Cohort Graduation Rate']
-"""
+
 
 class Output1:
     def __init__(self, master, return_dict, outcomes):
@@ -108,18 +108,20 @@ class miniFrame:
     def plots_frame(self):
         plots_frame = tk.Frame(self.state_frame, bg = "light blue")
         plots_frame.pack(anchor="n", fill="x", expand=False)
-        
+        canvas = tk.Canvas(plots_frame, width=1300, height=400)
+        canvas.grid(column = 0, row = 0)
+        sub_canvas_frame = tk.Frame(canvas)
+
         for outcome in self.outcomes:
+            figure = plt.Figure(figsize=(5,4), dpi=100)
             outcome_series = nces_trends[outcome]
             best_pol_series = avg_nctq[self.best_policy]
             #best_pol_series_abbr = ui_plot.abbrev_names(best_pol_series)
             rr_df = br.run_regression(best_pol_series, outcome_series, trend=True)
             #plot = ui_plot.scplot(best_pol_series_abbr, outcome_series, rr_df)
-
-            figure = plt.Figure(figsize=(5,4), dpi=100)
             ax = figure.add_subplot(111)
             ax.scatter(best_pol_series, outcome_series, color = 'g')
-            scatter = FigureCanvasTkAgg(figure, plots_frame) 
+            scatter = FigureCanvasTkAgg(figure, sub_canvas_frame) 
             scatter.get_tk_widget().pack(side=tk.LEFT, padx = 20, pady = 10)
             ax.legend(['State']) 
             ax.set_xlabel(best_pol_series.name, labelpad=15)
@@ -132,8 +134,20 @@ class miniFrame:
 
             figure.tight_layout(pad=3.0, w_pad=4.5, h_pad=3.0)
 
+        #canvas = FigureCanvasTkAgg(figure, master = plots_frame)
+        #canvas.get_tk_widget().pack(side=tk.LEFT, padx = 20, pady = 10)
 
-"""
+        canvas.create_window(0, 0, window = sub_canvas_frame, anchor = "nw")
+        plots_frame.update()
+        #Adding horizontal scrollbar to policies listbox
+        plots_scrollbarh = tk.Scrollbar(plots_frame, orient="horizontal")
+        plots_scrollbarh.config(command=canvas.xview)
+        canvas.config(xscrollcommand=plots_scrollbarh.set)
+        canvas.configure(scrollregion=canvas.bbox("all"))
+        plots_scrollbarh.grid(column = 0, row = 1, sticky = "NWE")
+
+
+
 # for testing purposes
 def main(): #run mianloop 
     master = tk.Tk()
@@ -148,4 +162,3 @@ def main(): #run mianloop
 
 if __name__ == '__main__':
     main()
-"""
